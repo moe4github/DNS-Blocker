ba1f58d555c0813305bcec6a229fdbdfc584d2b1
moe <moe@email.de>
2018-11-11 12:32:48 +0100

    add StevenBlack/hosts repo to project

--- make-bad-host-list.sh
+++ make-bad-host-list.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# update main repro
-echo "Update DNS-Blocker ... "
-git remote update https://github.com/moe4github/DNS-Blocker
+# update main repro via
+#git remote update https://github.com/moe4github/DNS-Blocker
 
-echo "done\n"
+# update init ad/bad-hosts
+bad_hosts="bad-hosts"
+if [ -d $bad_hosts ]
+    then
+        cd $bad_hosts
+        echo "Update Ad/Bad Hosts ... "
+        git remote update https://github.com/StevenBlack/hosts &> /dev/null
+        cd ..
+    else
+        echo "Init Ad/Bad Hosts ... "
+        git clone https://github.com/StevenBlack/hosts $bad_hosts &> /dev/null
+fi
+
+echo "done"
