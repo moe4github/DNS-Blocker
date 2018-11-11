1af59c9f1c164ba7c2bf5fb2f32c40b8497db13d
moe <moe@email.de>
2018-11-11 21:06:50 +0100

    generate bad-host.list file from hosts in bind9 syntax

--- make-bad-host-list.sh
+++ make-bad-host-list.sh
@@ -17,3 +17,28 @@ if [ -d $bad_hosts ]
 fi
 
 echo "done"
+
+# define resulting bad-hosts list file
+list="bad_hosts.list"
+serial=`date '+%Y%m%d%H'`
+
+# make bind file header
+cat <<EOT > $list
+;
+;   bind data file for rpz
+;
+\$TTL 604800
+@       IN  SOA     localhost. root.localhost. (
+                     $serial   ; serial
+                         604800   ; refresh
+                          86400   ; retry
+                        2419200   ; expire
+                         604800 ) ; negativ cache ttl
+;
+@               IN  NS  localhost.
+;
+EOT
+
+# convert hosts file to bind syntax
+grep -oP '^\d{1,3}\.0\.0\.\d\s+[^\s]+' bad-hosts/hosts | \
+sed -r "s/^(.*)\s(.*)$/\2\tA\t\1/g" >> $list
